edbcdee0fec86d658baec660e5b9726658920f97
ptwckkk1120 <gonzales.johnpatrickb@gmail.com>
2026-07-17 13:18:25 +0800

    Refactor company details layout in app.js

--- app.js
+++ app.js
@@ -3128,9 +3128,12 @@ function tabOutput(host, q){
         </div>
 
         <div class="doc-company-details">
-          <div class="co-meta">${escNl(COMPANY.address)}</div>
-          <div class="co-meta">${esc(COMPANY.contact)} &nbsp;·&nbsp; ${esc(COMPANY.email)}</div>
-          <div class="co-meta">TIN: ${esc(COMPANY.tin)}</div>
+          <div class="co-meta-col">
+            <div class="co-meta">${escNl(COMPANY.address)}</div>
+            <div class="co-meta">${esc(COMPANY.contact)} &nbsp;·&nbsp; ${esc(COMPANY.email)}</div>
+            <div class="co-meta">TIN: ${esc(COMPANY.tin)}</div>
+          </div>
+          <div class="doc-titlebar">QUOTATION</div>
         </div>
 
         <div class="doc-band-head">Client Information</div>
